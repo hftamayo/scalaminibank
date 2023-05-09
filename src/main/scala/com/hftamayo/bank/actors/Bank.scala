9285d0dfcb6e4db1be5feca80234b0bc34ef6340
hftamayo <hftamayo@gmail.com>
2023-05-09 16:51:12 -0600

    extending the functionality of Bank class part2

--- src/main/scala/com/hftamayo/bank/actors/Bank.scala
+++ src/main/scala/com/hftamayo/bank/actors/Bank.scala
@@ -1,8 +1,11 @@
 package com.hftamayo.bank.actors
 
+import akka.actor.typed.javadsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+
+import java.util.UUID
 
 class Bank {
 
@@ -18,16 +21,21 @@ class Bank {
   case class State(accounts: Map[String, ActorRef[Command]])
 
   //command handler
-  val commandHandler: (State, Command) => Effect[Event, State] = ???
+  def commandHandler: (context: ActorContext[Command]): (State, Command) => Effect[Event, State] = (state, command) =>
+    command match {
+      case CreateBankAccount(user, currency, initialBalance, replyTo) =>
+        val id = UUID.randomUUID().toString
+        val newBankAccount = context.spawn(PersistentBankAccount(id))
+    }
   //event handler
   val eventHandler: (State, Event) => State = ???
   //behavior
-  def apply(): Behavior[Command] =
+  def apply(): Behavior[Command] = Behaviors.setup { context =>
     EventSourcedBehavior[Command, Event, State](
       persistenceId = PersistenceId.ofUniqueId("bank"),
       emptyState = State(Map()),
-      commandHandler = commandHandler,
+      commandHandler = commandHandler(context),
       eventHandler = eventHandler
     )
-
+  }
 }
